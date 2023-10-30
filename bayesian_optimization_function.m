4f41df999a244c62b6756364315c3a3c729382d4
Kaige Tan <kaiget@UG.KTH.SE>
2023-10-30 13:17:45 +0100

    rm redundant file

--- bayesian_optimization_function.m
+++
@@ -1,7 +0,0 @@
-% Define the objective function for Bayesian optimization
-function obj = bayesian_optimization_function(x, X, y)
-    % Fit a Gaussian process regressor on the dataset
-    gpr = fitrgp(X, y, 'KernelFunction', 'squaredexponential', 'Sigma', 1e-5, 'BasisFunction', 'none');
-    % Predict the output for the given input
-    obj = -predict(gpr, x);
-end
